e6976a05a6feceb53926b71cd0908b81d2201481
janghyuan <janghuan@gmail.com>
2018-05-14 07:00:17 +0000

    add signup page and functions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
        page_title + ' | ' + base_title
      end
   end
+  
+  def gravatar_for user, options = {}
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size] || 36
+    klass = options[:class] || ''
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?size=#{size}"
+    image_tag(gravatar_url, alt: "#{user.name} avatar", class: "gravatar #{klass}")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get 'about', to: 'static_pages#about'
   get 'help', to: 'static_pages#help'
   get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
 
   root 'static_pages#home'
+  
+  resources :users
 end
